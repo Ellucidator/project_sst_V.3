c1b0ac7a10f59300385bbd1d9200969d24a3afa9
Marcelo Leal <andradefj13@hotmail.com>
2024-02-02 15:39:05 -0300

    update
    criação da tabela de usuarios e do modelo dela

---
+++ src/db/migrations/20240202175746-create-users-table.js
@@ -0,0 +1,74 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      first_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      last_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      birth: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.BLOB,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: 'user'
+      },
+      img_key: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      bucket: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      mime: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
---
+++ src/db/models/User.ts
@@ -0,0 +1,80 @@
+import { Model, Optional, DataTypes } from "sequelize"
+import { sequelize } from "../index.js"
+
+
+export interface User{
+    id: number
+    first_name: string
+    last_name: string
+    phone: string
+    birth: Date
+    username: string
+    email: string
+    password: string
+    role:'admin'| 'user'
+    img_key:string
+    bucket:string
+    mime:string
+}
+
+export interface CreateUserAttributes extends Optional<User,'id'|'img_key'|'bucket'|'mime'>{}
+
+export interface UserInstance extends Model<User, CreateUserAttributes>, User {}
+
+export const User = sequelize.define<UserInstance,User>('User', {
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    first_name:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    last_name:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    phone:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    birth:{
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    username:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    email:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password:{
+        type: DataTypes.BLOB,
+        allowNull: false
+    },
+    role:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    img_key:{
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    bucket:{
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    mime:{
+        type: DataTypes.STRING,
+        allowNull: true
+    }
+},{
+    tableName: 'users',
+
+},)
