53fc381221d2ac28d0db1068ce79fe03ffecb881
Marcelo Leal <andradefj13@hotmail.com>
2024-02-03 16:04:08 -0300

    update
    implementação do metodo get que retorna uma categoria e todas as subcategorias associadas a ela

--- src/db/controllers/categorieController.ts
+++ src/db/controllers/categorieController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { Category } from "../models/index.js";
+import { Category, SubCategory } from "../models/index.js";
 
 
 export const categoriesController = {
@@ -8,6 +8,18 @@ export const categoriesController = {
         try {
             const categories = await Category.findAll({order: [['position', 'ASC']]});
             return res.json(categories)
+        } catch (error) {
+            if(error instanceof Error) {
+                throw new Error(error.message)
+            }
+        }
+    },
+
+    getOneCategoryAndSubCategories: async (req:Request,res:Response) => {
+        try {
+            const categoryId = req.params.id
+            const category = await Category.findOne({where: {id: categoryId},include:{model:SubCategory}})
+            return res.json(category)
         } catch (error) {
             if(error instanceof Error) {
                 throw new Error(error.message)
--- src/routes.ts
+++ src/routes.ts
@@ -4,5 +4,6 @@ import { categoriesController } from './db/controllers/categorieController.js';
 const router = express.Router();
 
 router.get('/categories',categoriesController.list)
+router.get('/categories/:id',categoriesController.getOneCategoryAndSubCategories)
 
 export default router
