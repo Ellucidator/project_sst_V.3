fe8d0fb5edc8f23c19eb0e49ce92096e58b684aa
Marcelo Leal <andradefj13@hotmail.com>
2024-02-07 15:43:24 -0300

    update
    implementação no metodo de promotions, a ideia é entregar uma tabela com a promoção de cada item, para ter acesso ao preço na promoção e nornal

--- src/db/controllers/itemController.ts
+++ src/db/controllers/itemController.ts
@@ -5,54 +5,54 @@ import { itemServices } from "../services/itemServices.js";
 
 export const itemController = {
 
-    highlighted: async (req:Request, res:Response)=>{
+    highlighted: async (req: Request, res: Response) => {
 
         try {
-            const itemsHighlighted = await Item.findAll({where: {featured: true},attributes: ['id', 'name', 'price','description', 'in_stock', 'thumbnail_url','images']})
+            const itemsHighlighted = await Item.findAll({ where: { featured: true }, attributes: ['id', 'name', 'price', 'description', 'in_stock', 'thumbnail_url', 'images'] })
             return res.json(itemsHighlighted)
         } catch (error) {
-            if(error instanceof Error) {
+            if (error instanceof Error) {
                 throw new Error(error.message)
             }
         }
     },
 
-    promotion: async (req:Request, res:Response)=>{
+    promotion: async (req: Request, res: Response) => {
         try {
-            const itemsPromotin = await Item.findAll({where: {promotion: true},attributes: ['id', 'name', 'price','description', 'in_stock', 'thumbnail_url','images']})
+            const itemsPromotin = await Item.findAll({ where: { promotion: true }, attributes: ['id', 'name', 'price', 'description', 'in_stock', 'thumbnail_url', 'images'],include:{association:'Promotion'}})
             return res.json(itemsPromotin)
         } catch (error) {
-            if(error instanceof Error) {
-                res.status(500).json({error: error.message})
+            if (error instanceof Error) {
+                res.status(500).json({ error: error.message })
             }
         }
     },
 
-    show:async (req:Request, res:Response)=>{
+    show: async (req: Request, res: Response) => {
         try {
             const itemId = req.params.id
-            const item = await Item.findOne({where:{id:itemId},attributes: ['id', 'name', 'price','description', 'in_stock','promotion', 'thumbnail_url','images']})
+            const item = await Item.findOne({ where: { id: itemId }, attributes: ['id', 'name', 'price', 'description', 'in_stock', 'promotion', 'thumbnail_url', 'images'] })
 
             return res.json(item)
         } catch (error) {
-            if(error instanceof Error) {
-                res.status(500).json({error: error.message})
+            if (error instanceof Error) {
+                res.status(500).json({ error: error.message })
             }
         }
     },
 
-    search:async(req:Request,res:Response)=>{
+    search: async (req: Request, res: Response) => {
         try {
-            const {name}= req.query
+            const { name } = req.query
 
-            if(!name) return res.status(400).json({error: 'name is required'})
-            
+            if (!name) return res.status(400).json({ error: 'name is required' })
+
             const items = await itemServices.findByName(name.toString())
 
             return res.json(items)
         } catch (error) {
-            if(error instanceof Error) {
-                res.status(500).json({error: error.message})
+            if (error instanceof Error) {
+                res.status(500).json({ error: error.message })
             }
         }
     }
