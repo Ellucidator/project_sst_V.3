477e6138a235ce468fa45774cbbe85a825b25fbd
Marcelo Leal <andradefj13@hotmail.com>
2024-02-08 09:51:01 -0300

    update
    implementação do metodo de registro de usuario

--- src/db/controllers/authController.ts
+++ src/db/controllers/authController.ts
@@ -30,5 +30,25 @@ export const authController = {
                 res.status(500).json({error: error.message})
             }
         }
+    },
+
+    register:async (req:Request,res:Response)=>{
+        try {
+            const {email, password, username, first_name, last_name, phone, birth} = req.body
+            const user = await User.create({email, password, username, first_name, last_name, phone, birth,role:'user'})
+            if(user){
+                const payload = {
+                    id: user.id,
+                    email: user.email,
+                }
+                const token = jwtService.generateToken(payload, '1h')
+                return res.status(201).json({authenticated: true, user, token})
+            }
+            return res.status(500).json({error: 'Error creating user'})
+        } catch (error) {
+            if(error instanceof Error) {
+                res.status(500).json({error: error.message})
+            }
+        }
     }
 }
