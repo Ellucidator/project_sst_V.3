59c80d5050ef53c440666123b8e9a1f159435820
Marcelo Leal <andradefj13@hotmail.com>
2024-02-09 09:06:42 -0300

    update
    adicionando paginação a requisição de items da subclasse

--- src/db/controllers/subCategoriesController.ts
+++ src/db/controllers/subCategoriesController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { SubCategory } from "../models/index.js";
+import { getPaginationParams } from "src/helpers/getPaginationParams.js";
 
 
 export const subCategoriesControllers = {
@@ -7,9 +8,12 @@ export const subCategoriesControllers = {
         try {
             const subCategoryId = req.params.id
 
-            const subCategory = await SubCategory.findOne({where: {id:subCategoryId},attributes: ['id', 'name'],include:{association:'Items',attributes: ['id', 'name', 'price', 'thumbnail_url']}})
+            const [page, perPage] = getPaginationParams(req.query)
+            const offset = (page - 1) * perPage
 
-            return res.json(subCategory)
+            const {count, rows} = await SubCategory.findAndCountAll({where: {id:subCategoryId},attributes: ['id', 'name'],include:{association:'Items',attributes: ['id', 'name', 'price', 'thumbnail_url']},limit:perPage,offset})
+
+            return res.json({count, rows, page, perPage})
 
         } catch (error) {
             if(error instanceof Error) {
