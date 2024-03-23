612c1bc6685560965b47c78913a4b6d3153a5317
Marcelo Leal <andradefj13@hotmail.com>
2024-03-23 15:05:09 -0300

    update
    Mudança na requisição de sub categoria com itens

--- src/db/controllers/subCategoriesController.ts
+++ src/db/controllers/subCategoriesController.ts
@@ -5,28 +5,27 @@ import { Item, SubCategory } from "../models/index.js";
 export const subCategoriesControllers = {
     subCategoryAndItems: async (req:Request, res: Response)=>{
         try {
+
             const subCategoryId = req.params.id
-
-            const subCategory = await SubCategory.findOne({where: {id:subCategoryId}})
-
-            const subCategoryItems = await Item.findAll({
-                where: { sub_category_id: subCategoryId },
-                attributes:['id', 'name', 'price','promotion', 'description', 'in_stock', 'thumbnail_url'],
-                include:[
-                    {
+            const subCategory = await SubCategory.findOne({
+                where: {id: subCategoryId},
+                attributes: ['id', 'name'],
+                include:[{
+                    association:'Items',
+                    attributes:['id', 'name', 'price','promotion', 'description', 'in_stock', 'thumbnail_url'],
+                    order:[['createdAt', 'DESC']],
+                    include:[{
                         association:'ItemPromotion',
                         attributes:['price']
-                    }
-                ],
-                order:[['createdAt', 'ASC']],
+                    }]
+                }]
             })
-            if(!subCategory) {
-                return res.status(200).json('Esta sub categoria não tem itens')
-            }
-            const subCaregoryItemsRes = {
+            if(!subCategory) return res.status(404).json({error: 'subCategory not found'})
+
+            const subCategoryRes={
                 id: subCategory.id,
                 name: subCategory.name,
-                items: subCategoryItems.map(item=>{
+                items: subCategory.Items?.map(item => {
                     if(item.ItemPromotion){
                         return {
                             id: item.id,
@@ -51,8 +50,7 @@ export const subCategoriesControllers = {
                     }
                 })
             }
-            return res.json(subCaregoryItemsRes)
-
+            res.json(subCategoryRes)
         } catch (error) {
             if(error instanceof Error) {
                 res.status(500).json({error: error.message})
