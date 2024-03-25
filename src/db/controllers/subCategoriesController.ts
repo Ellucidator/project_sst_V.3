47d5745913b429fbff7808130566824dd9cbba28
Marcelo Leal <andradefj13@hotmail.com>
2024-03-25 17:13:27 -0300

    update
    solução temporaria para impor o offset a tabela incluida

--- src/db/controllers/subCategoriesController.ts
+++ src/db/controllers/subCategoriesController.ts
@@ -1,32 +1,48 @@
 import { Request, Response } from "express";
 import { Item, SubCategory } from "../models/index.js";
+import { Op } from "sequelize";
+import { sequelize } from "../index.js";
 
 
 export const subCategoriesControllers = {
-    subCategoryAndItems: async (req:Request, res: Response)=>{
+    subCategoryAndItems: async (req: Request, res: Response) => {
         try {
 
             const subCategoryId = req.params.id
             const subCategory = await SubCategory.findOne({
-                where: {id: subCategoryId},
+                where: { id: subCategoryId },
                 attributes: ['id', 'name'],
-                include:[{
-                    association:'Items',
-                    attributes:['id', 'name', 'price','promotion', 'description', 'in_stock', 'thumbnail_url'],
-                    order:[['createdAt', 'DESC']],
-                    include:[{
-                        association:'ItemPromotion',
-                        attributes:['price']
+                include: [{
+                    model: Item,
+                    attributes: ['id', 'name', 'price', 'promotion', 'description', 'in_stock', 'thumbnail_url'],
+                    where: {
+                        id: {
+                            [Op.in]: [
+                                sequelize.literal(`(
+                                    SELECT id
+                                    FROM items
+                                    WHERE sub_category_id = ${subCategoryId}
+                                    ORDER BY created_at DESC
+                                    OFFSET ${0}
+                                    LIMIT ${10}
+                                    )`)
+                            ]
+                        }
+                    },
+                    include: [{
+                        association: 'ItemPromotion',
+                        attributes: ['price']
                     }]
-                }]
-            })
-            if(!subCategory) return res.status(404).json({error: 'subCategory not found'})
+                }],
 
-            const subCategoryRes={
+            })
+            if (!subCategory) return res.status(404).json({ error: 'subCategory not found' })
+
+            const subCategoryRes = {
                 id: subCategory.id,
                 name: subCategory.name,
                 items: subCategory.Items?.map(item => {
-                    if(item.ItemPromotion){
+                    if (item.ItemPromotion) {
                         return {
                             id: item.id,
                             name: item.name,
@@ -37,7 +53,7 @@ export const subCategoriesControllers = {
                             promotion: item.promotion,
                             price_promotion: item.ItemPromotion.price
                         }
-                    }else{
+                    } else {
                         return {
                             id: item.id,
                             name: item.name,
@@ -52,8 +68,8 @@ export const subCategoriesControllers = {
             }
             res.json(subCategoryRes)
         } catch (error) {
-            if(error instanceof Error) {
-                res.status(500).json({error: error.message})
+            if (error instanceof Error) {
+                res.status(500).json({ error: error.message })
             }
         }
     }
