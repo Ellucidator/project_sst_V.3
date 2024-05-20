aa5766e204450f062a8ad6bb80820d1d52d9b7e8
Marcelo Leal <andradefj13@hotmail.com>
2024-05-20 14:44:34 -0300

    update
    Modificações na função de consultar os favoritos de um usuario para se adecuar ao necessario

--- src/db/controllers/userController.ts
+++ src/db/controllers/userController.ts
@@ -1,56 +1,79 @@
 import { Request, Response } from "express";
-import {  User } from "../models/index.js";
+import { Favorite, User } from "../models/index.js";
 import { AuthenticatedRequest } from "../middlewares/auth.js";
 import { userServices } from "../services/userServices.js";
 import { CreateUserAttributes } from "../models/User.js";
+import { getPaginationParams } from "src/helpers/getPaginationParams.js";
 
 
 export const userController = {
-    createUser:async (req:Request,res:Response)=>{
+    createUser: async (req: Request, res: Response) => {
         try {
-            const {first_name, last_name, email,password, phone, birth}:CreateUserAttributes = req.body
+            const { first_name, last_name, email, password, phone, birth }: CreateUserAttributes = req.body
             const role = 'user'
-            const newUser = await User.create({first_name, last_name, email, password, phone, birth, role})
-            
+            const newUser = await User.create({ first_name, last_name, email, password, phone, birth, role })
+
 
             return res.status(201).json(newUser)
 
         } catch (error) {
-            if(error instanceof Error) {
-                
-                res.status(500).json({error: error.message})
+            if (error instanceof Error) {
+
+                res.status(500).json({ error: error.message })
             }
         }
     },
-    show:async (req:AuthenticatedRequest,res:Response)=>{
+    show: async (req: AuthenticatedRequest, res: Response) => {
 
         try {
             const userId = req.user!.id
 
-            const user = await User.findOne({where: {id: userId}, attributes: ['id', 'email', 'first_name', 'last_name', 'phone', 'birth','img_key']})
+            const user = await User.findOne({ where: { id: userId }, attributes: ['id', 'email', 'first_name', 'last_name', 'phone', 'birth', 'img_key'] })
 
             return res.json(user)
         } catch (error) {
-            if(error instanceof Error) {
-                res.status(500).json({error: error.message})
+            if (error instanceof Error) {
+                res.status(500).json({ error: error.message })
             }
         }
     },
 
-    showFavorites:async(req:AuthenticatedRequest,res:Response)=>{
+    showFavorites: async (req: AuthenticatedRequest, res: Response) => {
         try {
+
+            const [page, perPage] = getPaginationParams(req.query)
+
             const userId = req.user!.id
-            const favorites = await userServices.favorites(userId)
+            const favorites = await Favorite.findAll(
+                {
+                    where: { user_id: userId },
+                    limit: perPage,
+                    offset: (page - 1) * perPage,
+                    attributes:[],
+                    include: [
+                        {
+                            association: 'Item',
+                            attributes: ['id', 'name', 'price', 'description', 'in_stock', 'promotion', 'thumbnail_url', 'sub_category_id'],
+                            include: [
+                                {
+                                    association: 'ItemPromotion',
+                                    attributes: ['price']
+                                }
+                            ]
+                        }
+                    ]
+                }
+            )
 
             return res.json(favorites)
         } catch (error) {
-            if(error instanceof Error) {
-                res.status(500).json({error: error.message})
+            if (error instanceof Error) {
+                res.status(500).json({ error: error.message })
             }
         }
     },
 
-    addFavorite:async(req:AuthenticatedRequest,res:Response)=>{
+    addFavorite: async (req: AuthenticatedRequest, res: Response) => {
 
         try {
             const userId = req.user!.id
@@ -60,16 +83,16 @@ export const userController = {
 
             return res.json(favorite)
         } catch (error) {
-            if(error instanceof Error) {
-                res.status(500).json({error: error.message})
+            if (error instanceof Error) {
+                res.status(500).json({ error: error.message })
             }
         }
     },
 
-    deleteFavorite:async(req:AuthenticatedRequest,res:Response)=>{
+    deleteFavorite: async (req: AuthenticatedRequest, res: Response) => {
 
         try {
-            
+
             const userId = req.user!.id
             const itemId = req.body.itemId
 
@@ -78,8 +101,8 @@ export const userController = {
             return res.status(204).json('Favorite deleted')
 
         } catch (error) {
-            if(error instanceof Error) {
-                res.status(500).json({error: error.message})
+            if (error instanceof Error) {
+                res.status(500).json({ error: error.message })
             }
         }
     }
--- src/db/services/userServices.ts
+++ src/db/services/userServices.ts
@@ -2,35 +2,7 @@ import { Favorite } from "../models/index.js"
 
 
 export const userServices = {
-    favorites: async (userId: number) => {
-        const favorites = await Favorite.findAll({
-            where:{user_id:userId},
-            include:[{
-                association:'Item',
-                attributes: ['id', 'name', 'price', 'in_stock', 'thumbnail_url', 'promotion'],
-                include:[{
-                    association:'Promotion',
-                    attributes: ['price']
-                }]
-            }]
-        })
-        return {
-            userId,
-            favorites: favorites.map(favorite=>{
-                if(favorite.Item!.promotion){
-                    return favorite.Item
-                }else{
-                    return {
-                        id: favorite.Item!.id,
-                        name: favorite.Item!.name,
-                        price: favorite.Item!.price,
-                        in_stock: favorite.Item!.in_stock,
-                        thumbnail_url: favorite.Item!.thumbnail_url
-                    }
-                }
-            })
-        }
-    },
+
 
     addFavorite: async (userId: number, itemId: number) => {
         const favorite = await Favorite.create({ user_id: userId, item_id: itemId })
