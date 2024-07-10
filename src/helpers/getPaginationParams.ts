eb79cb09771fb53f1eb956c1b1c30f7c5647fd83
Marcelo Leal <andradefj13@hotmail.com>
2024-07-10 16:27:38 -0300

    update
    mudança no helper de pagination, agora não é mais necessario retornar a page já que ele já retorna o offset, e passa a tratar a order também

--- src/helpers/getPaginationParams.ts
+++ src/helpers/getPaginationParams.ts
@@ -1,6 +1,6 @@
 
-export function getPaginationParams(query: any): [page: number, perPage: number] {
-    const { page, perPage } = query
+export function getPaginationParams(query: any): [ perPage: number,offset: number, order: string[]] {
+    const { page, perPage,order } = query
 
     const perPageNumber = typeof perPage === 'string' && parseInt(perPage, 10) > 0
         ? parseInt(perPage, 10)
@@ -9,6 +9,12 @@ export function getPaginationParams(query: any): [page: number, perPage: number]
     const pageNumber = typeof page === 'string' && parseInt(page, 10) > 0
         ? parseInt(page, 10)
         : 1
+    
+    const offset = (pageNumber - 1) * perPageNumber
 
-    return [pageNumber, perPageNumber]
+    const orderQ = typeof order === 'string' 
+    ? order.split('-') 
+    : ['created_at', 'DESC'] 
+    
+    return [perPageNumber,offset, orderQ]
 }
