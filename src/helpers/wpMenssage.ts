44fb3e472af936b0d952adf8e7a5ed492982abbf
Marcelo Leal <andradefj13@hotmail.com>
2024-09-30 15:01:58 -0300

    #feat
    implementação de mensagens template

--- src/helpers/wpMenssage.ts
+++ src/helpers/wpMenssage.ts
@@ -1,8 +1,15 @@
-interface Props {
+interface TextMenssage {
     to: string
     body: string
 }
-export async function wpMenssage({to,body}:Props) {
+
+interface TemplateMenssage {
+    to: string,
+    name: string,
+    variables?:string[],
+    dinamicUrl?: string|number
+}
+export async function wpMenssage({to,body}:TextMenssage) {
     await fetch(`https://graph.facebook.com/v20.0/${process.env.WP_NUMBER_ID}/messages`, {
         headers: {
             'Content-Type': 'application/json',
@@ -18,6 +25,46 @@ export async function wpMenssage({to,body}:Props) {
                 preview_url: true,
                 body
             }
+        })
+    })
+}
+
+export async function wpMenssageTemplate({to,name,variables,dinamicUrl}:TemplateMenssage) {
+    await fetch(`https://graph.facebook.com/v20.0/${process.env.WP_NUMBER_ID}/messages`, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.WP_TOKEN}`
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            messaging_product: 'whatsapp',
+            recipient_type: 'individual',
+            to: `55${to}`,
+            type: 'template',
+            template:{
+                name,
+                language:{
+                    code:'pt_BR'
+                },
+                components:[
+                    {
+                        type:'body',
+                        parameters: variables?.map(variable => ({type:'text',text: variable}))
+                    },
+                    {
+                        type:'button',
+                        sub_type:'URL',
+                        index:0,
+                        parameters:[
+                            {
+                                type:'text',
+                                text:dinamicUrl
+                            }
+                        ]
+                    }
+                ]
+            }
+            
         })
     })
 }
